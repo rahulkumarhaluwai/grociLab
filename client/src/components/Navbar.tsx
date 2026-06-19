c609abb2be3417e51b84856cb3d5784da53189af
rahul <rahulkumarhaluwai@gmail.com>
2026-06-19 11:14:46 +0545

    navbar update

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,45 @@
+import { BikeIcon, SearchIcon, ShoppingCartIcon } from 'lucide-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const user:any={name:"John Doe", email:"john@example.com", isAdmin:true}
+    const {cartCount, setIsCartOpen} = {
+      cartCount:5,
+      setIsCartOpen:(_data:any)=>{}
+    };
+    const [searchQuery, setSearchQuery] = useState("")
+    const [userMenuOpen, setUserMenuOpen] = useState(false)
+    const navigate = useNavigate()
+
   return (
-    <div>
-      
-    </div>
+    <nav className="bg-white sticky top-0 z-50 border-b border-app-border">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-16 gap-4">
+        <Link to="/" className="flex items-center gap-2 text-[22px] font-medium shrink-0">
+        <BikeIcon size={24}/>Instacart
+        </Link>
+        <div className="w-full flex items-center justify-end gap-4 lg:gap-10">
+          <div className="hidden md:flex items-center gap-6 text-sm text-zinc-600">
+             <Link to="/">Home</Link>
+             <Link to="/products">Products</Link>
+             <Link to="/deals" className="text-app-orange">Deals</Link>
+          </div>
+          <form className="hidden sm:flex flex-1 max-w-sm text-sm text-xs sm:text-sm">
+            <div className="relative w-full">
+              <SearchIcon className="absolute left-2.5 top-1/2 -translate-y-1/2 size-4 text-zinc-500"/>
+              <input type='text' placeholder='Search for groceries...' value={searchQuery} onChange={(e) =>setSearchQuery(e.target.value)}
+              className='w-full pl-8 p-2 bg-orange/15 focus:ring-app-orange/30'/>                 
+            </div>
+          </form>
+          <div className="flex items-center gap-3">
+           <button>
+            <ShoppingCartIcon className='size-5 text-zinc-900'/>
+            {cartCount >0 && <span>{cartCount}</span>}
+           </button>
+        </div>
+      </div>
+      </div>
+    </nav>
   )
 }
 
