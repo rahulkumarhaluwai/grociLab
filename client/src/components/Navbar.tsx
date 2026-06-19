37fffcae8a61527f62da87f1bce6d9f83346fae6
rahul <rahulkumarhaluwai@gmail.com>
2026-06-19 23:55:18 +0545

    home page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,9 @@
-import { BikeIcon, SearchIcon, ShoppingCartIcon } from 'lucide-react';
+import { ArrowUpRightIcon, BikeIcon, ChevronDownIcon, LogOutIcon, MapPinIcon, MenuIcon, PackageIcon, SearchIcon, ShieldIcon, ShoppingCartIcon, UserIcon, XIcon } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const user:any={name:"John Doe", email:"john@example.com", isAdmin:true}
+  const user:any=null;
     const {cartCount, setIsCartOpen} = {
       cartCount:5,
       setIsCartOpen:(_data:any)=>{}
@@ -11,6 +11,19 @@ const Navbar = () => {
     const [searchQuery, setSearchQuery] = useState("")
     const [userMenuOpen, setUserMenuOpen] = useState(false)
     const navigate = useNavigate()
+
+    const handleSearch=(e:React.SubmitEvent)=>{
+      e.preventDefault()
+      if(searchQuery.trim()){
+        navigate(`/search?q=${encodeURIComponent(searchQuery.trim())}`)
+        setSearchQuery("")
+      }
+    }
+
+    const handleLogout=()=>{
+      setUserMenuOpen(false)
+      navigate("/");
+    }
 
   return (
     <nav className="bg-white sticky top-0 z-50 border-b border-app-border">
@@ -24,7 +37,7 @@ const Navbar = () => {
              <Link to="/products">Products</Link>
              <Link to="/deals" className="text-app-orange">Deals</Link>
           </div>
-          <form className="hidden sm:flex flex-1 max-w-sm text-sm text-xs sm:text-sm">
+          <form onSubmit={handleSearch} className="hidden sm:flex flex-1 max-w-sm text-xs sm:text-sm">
             <div className="relative w-full">
               <SearchIcon className="absolute left-2.5 top-1/2 -translate-y-1/2 size-4 text-zinc-500"/>
               <input type='text' placeholder='Search for groceries...' value={searchQuery} onChange={(e) =>setSearchQuery(e.target.value)}
@@ -32,10 +45,64 @@ const Navbar = () => {
             </div>
           </form>
           <div className="flex items-center gap-3">
-           <button>
+           <button className="relative p-2 rounded-xl" onClick={()=>setIsCartOpen(true)}>
             <ShoppingCartIcon className='size-5 text-zinc-900'/>
-            {cartCount >0 && <span>{cartCount}</span>}
+            {cartCount >0 && <span className='absolute -top-1 -right-1 size-4 bg-app-orange text-white text-[10px] rounded-full flex-center'>{cartCount}</span>}
            </button>
+           <div className="relative">
+             {user ? (
+              <button onClick={()=>setUserMenuOpen(!userMenuOpen)} className="flex items-center gap-2 p-2">
+                <div className="size-7 rounded-full bg-green-950 text-white flex-center">
+                  {user.name.charAt(0).toUpperCase()}
+                </div>
+                 <ChevronDownIcon className='size-3 text-zinc-500'/>
+              </button>
+             ):(
+                <div className="flex-center gap-2">
+                  <Link to='/login' className="hidden md:flex items-center gap-2 px-4 py-2 text-sm font-medium text-medium text-white bg-green-950 rounded-full 
+                  hover:bg-green-950-light transition-colors">
+                  <UserIcon size={16}/> Sign In
+                  </Link>
+                  {userMenuOpen ? <XIcon className='md:hidden' onClick={()=> setUserMenuOpen(!userMenuOpen)}/>:
+                    <MenuIcon className='md:hidden' onClick={()=>setUserMenuOpen(!userMenuOpen)}/>}
+                </div>
+             )}
+             {userMenuOpen && (
+              <>
+              <div className="fixed inset-0 z-0" onClick={()=>setUserMenuOpen(false)}/>
+                <div className="absolute right-0 mt-2.5 w-56 bg-white rounded-xl shadow-lg border border-app-border py-2 z-50 animate-fade-in">
+                  {user &&(
+                    <div className='px-4 py-2 border-b border-app-border'>
+                      <p className="text-sm font-medium text-zinc-900">
+                        {user?.name}
+                      </p>
+                      <p className='text-xs text-zinc-500'>
+                        {user?.email}
+                      </p>
+                    </div>
+                  )}
+                  <div onClick={()=>setUserMenuOpen(false)}>
+                    {!user && <Link to="/login" className='dropdown-link'><UserIcon size={16}/>Sign In</Link>}
+                    {user && <Link to="/orders" className='dropdown-link'><PackageIcon size={16}/>My Orders</Link>}
+                    {user && <Link to="/addresses" className='dropdown-link'><MapPinIcon size={16}/>Addresses</Link>}
+                    <Link to="/products" className='dropdown-link md:hidden'><ArrowUpRightIcon size={16}/>Products</Link>
+                    <Link to="/deals" className='dropdown-link md:hidden'><ArrowUpRightIcon size={16}/>Deals</Link>
+                    {user?.isAdmin && (
+                      <Link to="/admin/products" className='dropdown-link'><ShieldIcon className='text-app-orange-dark' size={16}/>
+                      <span className='text-app-orange-dark'>Admin Panel</span></Link>
+                    )}
+                    {user && (
+                      <div className='border-t border-app-border pt-1'>
+                        <button onClick={handleLogout} className='flex items-center gap-3 px-4 py-2.5 text-sm text-app-error hover:bg-red-50 w-full transition-colors'>
+                          <LogOutIcon size={16}/>Logout
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </>
+             )}
+           </div>
         </div>
       </div>
       </div>
